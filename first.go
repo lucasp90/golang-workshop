09f4ee66c81d0ab79f1a79782e4077513a4c83b9
Lucas Perea <lperea@oficinacys.com.ar>
2017-06-09 11:53:01 -0300

    First exercise

---
+++ first.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func sumDivisible(n int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
+
+func main() {
+	n, err := strconv.Atoi(os.Args[1])
+	if err == nil {
+		fmt.Println(sumDivisible(int(n)))
+	}
+}
